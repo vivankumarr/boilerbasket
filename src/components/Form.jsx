2c6098d5270fe7c733c6dfbfd6057341cdc9fbd2
bjwdenbesten <bjwdenbesten@gmail.com>
2025-10-24 17:02:05 -0400

    booking comp

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 
 const Form = () => {
@@ -9,11 +9,59 @@ const Form = () => {
     const [email, setEmail] = useState('');
     const [role, setRole] = useState('');
 
+    //debugging
+    useEffect(() => {
+      console.log(name);
+      console.log(role);
+      console.log(puid);
+      console.log(email);
+    }, [name, role, puid, email]);
+
   return (
     <>
         <div className="bg-white shadow-2xl h-150 w-100 p-5 mt-10 mb-10 rounded-2xl">
             <span className="text-2xl">Book Your Appointment</span>
             <div className="flex flex-col items-center">
+              <div className="grid grid-cols-2 grid-rows-2 gap-4 mt-4">
+                  <div>
+                    <span>Full Name</span>
+                    <input onChange={(e) => {setName(e.target.value)}} className="border" type="text" />
+                  </div>
+                  <div>
+                    <span>Role</span>
+                    <select onChange={(e) => {setRole(e.target.value)}} className="border h-6.5 w-45" name="roles" id="">
+                      <option value="default"></option>
+                      <option value="student">Student</option>
+                      <option value="staff">Staff</option>
+                      <option value="other">Other</option>
+                    </select>
+                  </div>
+                  <div>
+                    <span>Email Address</span>
+                    <input onChange={(e) => {setEmail(e.target.value)}} className="border"type="text" />
+                  </div>
+                  <div>
+                    <span>PUID</span>
+                    <input onChange={(e) => {setPuid(e.target.value)}}className="border" type="text" />
+                  </div>
+              </div>
+
+              <div className="mt-5">
+                <span>Select Date</span>
+                <div id="Dates" className="w-full h-30">
+
+                </div>
+              </div>
+
+              <div className="mt-5">
+                <span>Select Time</span>
+                <div id="times" className="w-full h-30">
+
+                </div>
+              </div>
+
+              <button className="btn bg-secondary pt-2 pb-2 pr-1 pl-1 rounded-sm w-50 text-white">Confirm Booking</button>
+
             </div>
         </div>
     </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 "useclient";
 
-import React from 'react'
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <>
         <div className="flex flex-row items-center justify-center bg-lightprimary w-full h-15">
             <div className="flex h-full w-full justify-start items-center p-2">
-                <span className="ml-10">BoilerBasket</span>
+                <Image className="ml-5"src="/boilerbasket-logo.png" alt="" width={40} height={40}></Image>
+                <span className="ml-5">BoilerBasket</span>
             </div>
             <div className="flex h-full w-full justify-end items-center p-2">
                 <button className="btn mr-10">About</button>
