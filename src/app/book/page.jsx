75e88412cbf994ad5fd3cf644c066e2fdd3f3e27
Vivan <kumar733@purdue.edu>
2025-11-19 23:16:45 -0500

    Remove invalid times for pantry hours from time selector

--- src/app/book/page.jsx
+++ src/app/book/page.jsx
@@ -48,8 +48,8 @@ function makeSlots(blockedTimes, existingAppts) {
   nextMonth.setMonth(today.getMonth() + 1);
 
   //possible time slots, every 30 min
-  const timeSlotsTuesday = ['12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30', '18:00'];
-  const timeSlotsSunday = ['17:00', '17:30', '18:00', '18:30', '19:00', '19:30', '20:00'];
+  const timeSlotsTuesday = ['12:00', '12:30', '13:00', '13:30', '14:00', '14:30', '15:00', '15:30', '16:00', '16:30', '17:00', '17:30'];
+  const timeSlotsSunday = ['17:00', '17:30', '18:00', '18:30', '19:00', '19:30'];
 
   //max number of people we can have per time slot
   const maxPerTimeSlot = 5;
